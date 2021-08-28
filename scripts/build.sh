9adc4370adf20d1ee3622553affb91b1182c58be
Emmanuel Ndangurura <endangurura@tachyinc.com>
2021-08-28 17:02:37 +0300

    Updating dist files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf $PWD/dist
-babel src --out-dir dist --ignore '**/*-integration.js' --source-maps inline
+npx babel src --out-dir dist --ignore '**/*-integration.js' --source-maps inline
 git reset
 git add $PWD/dist
-git commit -m 'Updating dist files' -n
+git commit -am 'Updating dist files' -n
